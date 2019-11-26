c197917f2554ac1663d6a152de12baa539ca3674
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-26 14:58:25 -0500

    makefile stuff

---
+++ makefile
@@ -0,0 +1,7 @@
+ifeq ($DEBUG,true)
+	CFLAGS = -g
+endif
+all: parseargs.c
+	gcc $(CFLAGS) parseargs.c
+run:
+	./a.out
--- parseargs.c
+++ parseargs.c
@@ -3,21 +3,28 @@
 #include<string.h>
 #include<unistd.h>
 
-int main(){
-  printf("> ");
-  char buf[128];
-  unsigned short i,j;
-  fgets(buf,128,stdin);
-  char *args[64];
-  char *cur = buf;
+char ** parse_args(char *line) {
+  unsigned short i;
+  unsigned short ary_size = 4;
+  char **args = malloc(ary_size * sizeof(char *));
+  char *cur = line;
   i = 0;
   while(*cur != '\0') {
     args[i++] = strsep(&cur," \t\n");
+    if(i >= ary_size) {
+      ary_size *= 2;
+      args = realloc(args,ary_size * sizeof(char *));
+    }
   }
   args[i] = NULL;
-  // for(j = 0; j <= i; j++){
-  //   if(args[j] == NULL) printf("NULL PTR\n");
-  //   else                printf("[%s]\n",args[j]);
-  // }
+  return args;
+}
+
+int main(){
+  printf("> ");
+  char buf[128];
+  fgets(buf,128,stdin);
+  char **args = parse_args(buf);
   execvp(args[0],args);
+  free(args);
 }
