92b6755ea761890d172947f76099c6da9b19083f
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2019-11-26 13:19:03 -0500

    execvp on the generated array

--- parseargs.c
+++ parseargs.c
@@ -1,20 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
 
 int main(){
   printf("> ");
   char buf[128];
+  unsigned short i,j;
   fgets(buf,128,stdin);
   char *args[64];
   char *cur = buf;
-  unsigned short i = 0;
+  i = 0;
   while(*cur != '\0') {
     args[i++] = strsep(&cur," \t\n");
   }
-  unsigned short j;
-  for(j = 0; j <= i; j++){
-    if(args[j] == NULL) printf("NULL PTR\n");
-    else                printf("[%s]\n",args[j]);
-  }
+  args[i] = NULL;
+  // for(j = 0; j <= i; j++){
+  //   if(args[j] == NULL) printf("NULL PTR\n");
+  //   else                printf("[%s]\n",args[j]);
+  // }
+  execvp(args[0],args);
 }
